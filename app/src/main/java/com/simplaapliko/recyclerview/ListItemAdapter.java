f859aa2619b17a20000684afa60c87dd4f65533e
Oleg Kan <oleg.kan@hotmail.com>
2016-10-08 11:19:36 -0400

    Move data and ViewHolder to SelectableAdapter

--- app/src/main/java/com/simplaapliko/recyclerview/ListItemAdapter.java
+++ app/src/main/java/com/simplaapliko/recyclerview/ListItemAdapter.java
@@ -16,34 +16,29 @@
 
 package com.simplaapliko.recyclerview;
 
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import java.util.Arrays;
 import java.util.List;
 
-public class ListItemAdapter extends SelectableAdapter<ListItemAdapter.ViewHolder> {
+public class ListItemAdapter extends SelectableAdapter<ListItem, ListItemAdapter.ViewHolder> {
 
-    private List<ListItem> mData;
-    private ListItemAdapter.ViewHolder.ClickListener mClickListener;
-
-    public ListItemAdapter(List<ListItem> data, ListItemAdapter.ViewHolder.ClickListener clickListener) {
-        mData = data;
+    public ListItemAdapter(List<ListItem> data, SelectableAdapter.ViewHolder.ClickListener clickListener) {
+        setData(data);
         mClickListener = clickListener;
     }
 
     @Override
-    public ListItemAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item, parent, false);
         return new ViewHolder(view, mClickListener);
     }
 
     @Override
-    public void onBindViewHolder(ListItemAdapter.ViewHolder holder, int position) {
-        ListItem model = mData.get(position);
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        ListItem model = getData().get(position);
 
         holder.mNameView.setText(model.getName());
         holder.mDescriptionView.setText(model.getDescription());
@@ -53,65 +48,23 @@ public class ListItemAdapter extends SelectableAdapter<ListItemAdapter.ViewHolde
 
     @Override
     public int getItemCount() {
-        return mData.size();
+        return super.getItemCount();
     }
 
-    public void setData(List<ListItem> data) {
-        mData = data;
-    }
-
-    public void deleteSelectedItems() {
-        int[] selectedItems = getSelectedItems();
-        Arrays.sort(selectedItems);
-
-        for (int i = selectedItems.length - 1; i >= 0; i--) {
-            int position = selectedItems[i];
-            mData.remove(position);
-            notifyItemRemoved(position);
-        }
-    }
-
-    public static class ViewHolder extends RecyclerView.ViewHolder implements
-            View.OnClickListener, View.OnLongClickListener {
-
+    public static class ViewHolder extends SelectableAdapter.ViewHolder {
         View mSelectedOverlay;
         TextView mNameView;
         TextView mDescriptionView;
 
-        private ClickListener mClickListener;
-
         public ViewHolder(View itemView, ClickListener clickListener) {
-            super(itemView);
+            super(itemView, clickListener);
 
             mSelectedOverlay = itemView.findViewById(R.id.selected_overlay);
             mNameView = (TextView) itemView.findViewById(R.id.name);
             mDescriptionView = (TextView) itemView.findViewById(R.id.description);
 
-            mClickListener = clickListener;
-
             itemView.setOnClickListener(this);
             itemView.setOnLongClickListener(this);
-        }
-
-        @Override
-        public void onClick(View v) {
-            if (mClickListener != null) {
-                mClickListener.onItemClicked(getAdapterPosition());
-            }
-        }
-
-        @Override
-        public boolean onLongClick(View v) {
-            if (mClickListener != null) {
-                return mClickListener.onItemLongClicked(getAdapterPosition());
-            }
-            return false;
-        }
-
-        public interface ClickListener {
-            void onItemClicked(int position);
-
-            boolean onItemLongClicked(int position);
         }
     }
 }
--- app/src/main/java/com/simplaapliko/recyclerview/SelectableAdapter.java
+++ app/src/main/java/com/simplaapliko/recyclerview/SelectableAdapter.java
@@ -20,19 +20,31 @@ import android.support.v7.widget.RecyclerView;
 import android.util.SparseBooleanArray;
 import android.view.View;
 
-public abstract class SelectableAdapter<T extends RecyclerView.ViewHolder> extends RecyclerView.Adapter<T> {
+import java.util.Arrays;
+import java.util.List;
 
+public abstract class SelectableAdapter<S, T extends SelectableAdapter.ViewHolder> extends
+        RecyclerView.Adapter<T> {
+
+    protected ViewHolder.ClickListener mClickListener;
     private SparseBooleanArray mSelectedItems;
-
-    public static class ViewHolder extends RecyclerView.ViewHolder {
-
-        public ViewHolder(View itemView) {
-            super(itemView);
-        }
-    }
+    private List<S> mData;
 
     public SelectableAdapter() {
         mSelectedItems = new SparseBooleanArray();
+    }
+
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+    public List<S> getData() {
+        return mData;
+    }
+
+    public void setData(List<S> data) {
+        mData = data;
     }
 
     public boolean isSelected(int position) {
@@ -67,5 +79,47 @@ public abstract class SelectableAdapter<T extends RecyclerView.ViewHolder> exten
             selectedItems[i] = mSelectedItems.keyAt(i);
         }
         return selectedItems;
+    }
+
+    public void deleteSelectedItems() {
+        int[] selectedItems = getSelectedItems();
+        Arrays.sort(selectedItems);
+
+        for (int i = selectedItems.length - 1; i >= 0; i--) {
+            int position = selectedItems[i];
+            mData.remove(position);
+            notifyItemRemoved(position);
+        }
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder implements
+            View.OnClickListener, View.OnLongClickListener {
+
+        private ClickListener mClickListener;
+
+        public ViewHolder(View itemView, ClickListener clickListener) {
+            super(itemView);
+            mClickListener = clickListener;
+        }
+
+        @Override
+        public void onClick(View v) {
+            if (mClickListener != null) {
+                mClickListener.onItemClicked(getAdapterPosition());
+            }
+        }
+
+        @Override
+        public boolean onLongClick(View v) {
+            if (mClickListener != null) {
+                return mClickListener.onItemLongClicked(getAdapterPosition());
+            }
+            return false;
+        }
+
+        public interface ClickListener {
+            void onItemClicked(int position);
+            boolean onItemLongClicked(int position);
+        }
     }
 }
